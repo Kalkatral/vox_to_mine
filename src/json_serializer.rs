ade604dbbe26e10cc30c98b9dbae81ece9a1ab5f
Kalkatral <elsben0@gmail.com>
2021-10-17 18:34:29 +0200

    Implemented JsonSerializer::add_box and JsonSerializer::write_file

--- src/json_serializer.rs
+++ src/json_serializer.rs
@@ -1,4 +1,5 @@
-
+use std::fs::File;
+use std::io::Write;
 
 pub struct JsonSerializer
 {
@@ -10,21 +11,47 @@ impl JsonSerializer
     pub fn new(texture: &str, texture_size: (u8, u8)) -> Self
     {
         let mut new_string: String = String::new();
-        new_string.push_str("{\"texture\": {\"1\": \"");
+        new_string.push_str("{\n\t\"texture\": {\"1\": \"");
         new_string.push_str(texture);
-        new_string.push_str("\"},\"texture_size\": [");
+        new_string.push_str("\"},\n\t\"texture_size\": [");
         new_string.push_str(&(texture_size.0).to_string());
-        new_string.push_str(",");
+        new_string.push_str(", ");
         new_string.push_str(&(texture_size.1).to_string());
-        new_string.push_str("]}");
+        new_string.push_str("],\n\t\"elements\": [\n\t\t");
         Self
         {
             string: new_string,
         }
     }
 
-    pub fn add_box(&mut self, string: &str)
+    pub fn add_box(&mut self, start: (u8, u8, u8), end: (u8, u8, u8))
     {
-        self.string += string;
+        self.string.push_str("{\"name\": \"element\", \"from\": [");
+        self.string.push_str(&(start.0).to_string());
+        self.string.push_str(", ");
+        self.string.push_str(&(start.1).to_string());
+        self.string.push_str(", ");
+        self.string.push_str(&(start.2).to_string());
+        self.string.push_str("], \"to\": [");
+        self.string.push_str(&(end.0).to_string());
+        self.string.push_str(", ");
+        self.string.push_str(&(end.1).to_string());
+        self.string.push_str(", ");
+        self.string.push_str(&(end.2).to_string());
+        self.string.push_str("], \"faces\": {\"north\": {\"uv\": [0, 0, 16, 16], \"texture\": \"#1\"}}, ");
+        self.string.push_str("{\"south\": {\"uv\": [0, 0, 16, 16], \"texture\": \"#1\"}}, ");
+        self.string.push_str("{\"east\": {\"uv\": [0, 0, 16, 16], \"texture\": \"#1\"}}, ");
+        self.string.push_str("{\"west\": {\"uv\": [0, 0, 16, 16], \"texture\": \"#1\"}}, ");
+        self.string.push_str("{\"up\": {\"uv\": [0, 0, 16, 16], \"texture\": \"#1\"}}, ");
+        self.string.push_str("{\"down\": {\"uv\": [0, 0, 16, 16], \"texture\": \"#1\"}}},\n\t\t");
+    }
+
+    pub fn write_file(&mut self, filepath: &str) -> std::io::Result<()>
+    {
+        self.string.push_str("]\n}");
+
+        let mut file: File = File::create(filepath)?;
+        file.write(&self.string.clone().into_bytes())?;
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,24 @@
 mod json_serializer;
 use json_serializer::JsonSerializer;
 
-//const FILE_PATH: &str = "D:\\Docs\\Desktop\\Mess\\cup.vox";
+//const LOAD_FILE_PATH: &str = "D:\\Docs\\Desktop\\Mess\\cup.vox";
+const SAVE_FILE_PATH: &str = "D:\\Docs\\Desktop\\Mess\\cup.json";
 
 fn main()
 {
-    //let data: dot_vox::DotVoxData = dot_vox::load(FILE_PATH).expect("Failed to load .vox");
+    //let data: dot_vox::DotVoxData = dot_vox::load(LOAD_FILE_PATH).expect("Failed to load .vox");
 
     let mut json: JsonSerializer = JsonSerializer::new("item/test", (16, 16));
+    json.add_box((0, 0, 0), (6, 7, 1));
+    match json.write_file(SAVE_FILE_PATH)
+    {
+        Ok(()) =>
+        {
+            println!("Successfully saved json to {}", SAVE_FILE_PATH);
+        }
+        Err(e) =>
+        {
+            println!("Could not save json :\n{}", e);
+        }
+    }
 }
